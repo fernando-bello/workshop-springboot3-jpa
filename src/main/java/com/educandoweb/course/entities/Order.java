feda71c056f0fa0cad3f0d26c19ba06e3e8c292a
Fernando Bello <fernandobello700220@gmail.com>
2023-01-09 11:22:51 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -15,7 +16,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_order")
+@Table(name = "tb_order") // precisa nomear pois order pode causar confusão no banco de dados
 public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
 
@@ -26,8 +27,20 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'z'", timezone = "GMT")
 	private Instant moment;
 
-	public User getClient() {
-		return client;
+	private Integer orderStatus;
+
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private User client;
+
+	public Order() {
+	}
+
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+		this.id = id;
+		this.moment = moment;
+		setOrderStatus(orderStatus);
+		this.client = client;
 	}
 
 	public Long getId() {
@@ -46,20 +59,21 @@ public class Order implements Serializable {
 		this.moment = moment;
 	}
 
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
+
+	public User getClient() {
+		return client;
+	}
+
 	public void setClient(User client) {
-		this.client = client;
-	}
-
-	@ManyToOne
-	@JoinColumn(name = "client_id")
-	private User client;
-
-	public Order() {
-	}
-
-	public Order(Long id, Instant moment, User client) {
-		this.id = id;
-		this.moment = moment;
 		this.client = client;
 	}
 
